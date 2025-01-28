9fbf3925eaa879fb90e3807890357e4a6d6e34ca
Chen Xinjie <FoamValue@live.cn>
2025-01-28 22:10:18 +0800

    添加 gateway 模块

---
+++ mx-gateway/src/main/java/cn/chenxinjie/mx/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cn.chenxinjie.mx.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Configuration
+@EnableDiscoveryClient
+@RestController
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        System.out.println("GatewayApplication start");
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
